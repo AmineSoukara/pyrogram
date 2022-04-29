e10648a54e81aede8bb3dd8a13b434f6b7f11ae7
DAMIEN <65272894+AmineSoukara@users.noreply.github.com>
2022-04-29 01:35:20 +0000

    Update __init__.py

--- pyrogram/methods/messages/__init__.py
+++ pyrogram/methods/messages/__init__.py
@@ -15,7 +15,7 @@
 #
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
-
+from .iter_messages import IterMessages
 from .copy_media_group import CopyMediaGroup
 from .copy_message import CopyMessage
 from .delete_messages import DeleteMessages
@@ -97,6 +97,7 @@ class Messages(
     SendCachedMedia,
     GetChatHistoryCount,
     ReadChatHistory,
+    IterMessages,
     EditInlineText,
     EditInlineCaption,
     EditInlineMedia,
